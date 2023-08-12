57df5effc7cb6d734f888a02339df9dff3972ce1
albert-joonyoung-park <joonparkyoung@gmail.com>
2023-08-12 16:30:48 -0400

    start module-02 file

---
+++ module-02.py
@@ -0,0 +1,12 @@
+import os
+
+# create a file with data in it
+def create_file(filename)
+    with open(filename) as file:
+        file.wrtie("name,color,type\n")
+        file.write("\n")
+        file.write("\n")
+        file.write("\n")
+        file.write("\n")
+        file.write("\n")
+        
